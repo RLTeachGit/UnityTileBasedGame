45a02b4a2cd07f36e8a79588db7205a2193f8c63
Richard Leinfellner <richard@leinfellner.com>
2017-11-18 17:02:02 +0000

    Updated

---
+++ Assets/Physics/MoveTable.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTable : MonoBehaviour {
+
+    float tCurrent = 0.0f;
+    public float Speed = 10.0f;
+    Quaternion tOriginalRotation;
+
+    bool mShowSprites=true;
+
+    public  GameObject[] ToggleThese;
+
+
+	// Use this for initialization
+	void Start () {
+        tOriginalRotation = transform.rotation;
+        SetVisibility();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.rotation = tOriginalRotation * Quaternion.Euler(0, 0, MakeRotation(tCurrent)*20.0f);
+        tCurrent += Time.deltaTime;
+	}
+
+
+    float   MakeRotation(float tValue)
+    {
+        return  Mathf.Sin(tValue);
+    }
+
+    void SetVisibility()
+    {
+        foreach(GameObject tGO in ToggleThese)
+        {
+            tGO.GetComponent<SpriteRenderer>().enabled = mShowSprites;
+        }
+        mShowSprites =! mShowSprites;
+        Invoke("SetVisibility", 5.0f);
+    }
+
+}
